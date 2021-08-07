7333a0caf477e35c6e591a738eab060b117cc53d
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-08-07 11:59:56 +0530

    feat:LineComparison Program

---
+++ Line_Comparison_Oops/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Line_Comparison_Oops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Comparison Program!");
+
+            UC1_Length.getCartesian();
+            UC2_CheckLinesareEqual.getCartesian();
+            UC3_CheckTwoLines.getCartesian();
+            UC4_LineCompare obj = new UC4_LineCompare();
+            obj.input();
+            obj.cal();
+            obj.CompareLines();
+        }
+    }
+}
